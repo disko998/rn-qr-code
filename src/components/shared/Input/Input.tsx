286abe20ddcee2b03f58d5859131d29315df79a1
Stefan Diskic <disko998@gmail.com>
2021-01-25 14:28:22 +0100

    fix picker android

--- src/components/shared/Input/Input.tsx
+++ src/components/shared/Input/Input.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { TextInputProps, TouchableOpacityProps } from 'react-native'
+import { Platform, TextInputProps, TouchableOpacityProps } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select'
+import { Picker } from '@react-native-picker/picker'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 import {
@@ -44,26 +45,41 @@ Input.Select = ({ label, items, value, onValueChange }: SelectProps) => {
     <Container>
       {label && <Label>{label}</Label>}
       <InputWrapper>
-        <RNPickerSelect
-          placeholder={{ label: 'Select event' }}
-          onValueChange={onValueChange}
-          items={items}
-          value={value}
-          style={{
-            inputIOS: styles.inputPicker,
-            inputAndroid: styles.inputPicker,
-            inputAndroidContainer: styles.pickerContainer,
-            inputIOSContainer: styles.pickerContainer,
-            iconContainer: styles.iconContainer,
-          }}
-          Icon={() => (
-            <AntDesign
-              size={20}
-              name="caretdown"
-              color={appTheme.colors.darkGray}
-            />
-          )}
-        />
+        {Platform.OS === 'android' ? (
+          <Picker
+            onValueChange={onValueChange}
+            selectedValue={value}
+            style={styles.picker}
+            mode="dialog"
+            prompt="Events">
+            {items.map((item) => (
+              <Picker.Item
+                key={item.value}
+                label={item.label}
+                value={item.value}
+              />
+            ))}
+          </Picker>
+        ) : (
+          <RNPickerSelect
+            placeholder={{ label: 'Select event' }}
+            onValueChange={onValueChange}
+            items={items}
+            value={value}
+            style={{
+              inputIOS: styles.inputPicker,
+              inputIOSContainer: styles.pickerContainer,
+              iconContainer: styles.iconContainer,
+            }}
+            Icon={() => (
+              <AntDesign
+                size={20}
+                name="caretdown"
+                color={appTheme.colors.darkGray}
+              />
+            )}
+          />
+        )}
       </InputWrapper>
     </Container>
   )
@@ -82,7 +98,7 @@ type SelectProps = {
   label?: string
   items: DropDownItem[]
   value: string
-  onValueChange: (value: string) => void
+  onValueChange: (value: any) => void
 }
 
 export type DropDownItem = {
